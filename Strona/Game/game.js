6a94c6a86cf1f2e3d5bc68b7d9b0839a129af6fb
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-05-12 22:46:56 +0200

    Cat and bomb appearing in random place every second

--- Strona/Game/game.js
+++ Strona/Game/game.js
@@ -9,8 +9,9 @@ function createBomb() {
   createBombElement.setAttribute("src", "images/bomb.png");
   createBombElement.classList.add("bomb");
   grabGameWindow.appendChild(createBombElement);
+  giveBombTopAndLeftPositionValue();
 }
-createBomb();
+// createBomb();
 
 function deleteBomb() {
   grabGameWindow.removeChild(createBombElement);
@@ -21,8 +22,9 @@ function createCat() {
   createCatElement.setAttribute("src", "images/Cat-icon.png");
   createCatElement.classList.add("cat");
   grabGameWindow.appendChild(createCatElement);
+  giveCatTopAndLeftPositionValue();
 }
-createCat();
+// createCat();
 
 function deleteCat() {
   grabGameWindow.removeChild(createCatElement);
@@ -53,3 +55,18 @@ bombAddEventListener();
 function showYouLoseDiv() {
   loseWindow.style.zIndex = "2";
 }
+
+function giveCatTopAndLeftPositionValue(){
+  let cat = document.querySelector('.cat');
+  cat.style.top = (Math.floor(Math.random() * 500) + 80) + 'px';
+  cat.style.left = (Math.floor(Math.random() * 800) + 50) + 'px';
+}
+function giveBombTopAndLeftPositionValue(){
+  let bomb = document.querySelector('.bomb');
+  bomb.style.top = (Math.floor(Math.random() * 500) + 80) + 'px';
+  bomb.style.left = (Math.floor(Math.random() * 800) + 50) + 'px';
+}
+
+setInterval(createBomb, 1000);
+setInterval(createCat, 1000);
+
